107ba99e5e9d15ee6dc6c21a1b87c7f322e1f66b
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-29 19:30:04 +0800

    Nixify

---
+++ lazy-functional-state-threads/default.nix
@@ -0,0 +1,17 @@
+let
+  inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  versions = lib.mapAttrs
+    (_: fetchFromGitHub)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
+  pkgs = import versions.nixpkgs {};
+in pkgs.runCommand "lazy-functional-state-threads" {
+  buildInputs = [
+    pkgs.haskellPackages.pandoc
+    pkgs.python3
+  ];
+  src = ./.;
+} ''
+  mkdir -p $out
+  pandoc --standalone -t revealjs -V theme:simple $src/presentation.md -o $out/index.html
+  cp -R ${versions.revealjs} $out/reveal.js
+''
