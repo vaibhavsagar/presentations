2e64479aaa8f14ab171be76c8675ff732b1fed95
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-29 17:37:13 +0800

    Nixify IHaskell notebook setup

---
+++ imperative-haskell/default.nix
@@ -0,0 +1,17 @@
+let
+  inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  versions = lib.mapAttrs
+    (_: fetchFromGitHub)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
+  # ./updater versions.json ihaskell
+  IHaskell = versions.ihaskell;
+  # ./updater versions.json nixpkgs nixos-17.09
+  pinned   = versions.nixpkgs;
+in import "${IHaskell}/release.nix" {
+  nixpkgs = import pinned {};
+  packages = self: with self; [
+    containers
+    unordered-containers
+    vector
+  ];
+}
