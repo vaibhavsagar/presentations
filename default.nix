47c3cd37ab02c6593c0cafa3e9b3a38f2dbec6f1
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-29 21:19:45 +0800

    Build link farm of presentations

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  inherit (import <nixpkgs> {}) linkFarm;
+  presentations = map (folder: {
+    name = folder;
+    path = import (./. + "/${folder}");
+  });
+in linkFarm "presentations" (presentations [
+  "functional-devops"
+  "lazy-functional-state-threads"
+  "tarjans-scc"
+])
