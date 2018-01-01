1a0f4aeb3ebba348485f179c391cb1933723bc2b
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-01-01 21:23:37 +0800

    nix/default.nix: simplify

--- nix/default.nix
+++ nix/default.nix
@@ -4,13 +4,13 @@ let
     (_: fetchFromGitHub)
     (builtins.fromJSON (builtins.readFile ./versions.json));
   pkgs = import versions.nixpkgs {};
-in name: src: let input = src + "/presentation.md"; in pkgs.runCommand name {
+in name: src: pkgs.runCommand name {
   buildInputs = [
     pkgs.haskellPackages.pandoc
     pkgs.python3
   ];
 } ''
   mkdir -p $out
-  pandoc --standalone -t revealjs -V theme:simple ${input} -o $out/index.html
+  pandoc --standalone -t revealjs -V theme:simple ${src + "/presentation.md"} -o $out/index.html
   cp -R ${versions.revealjs} $out/reveal.js
 ''
