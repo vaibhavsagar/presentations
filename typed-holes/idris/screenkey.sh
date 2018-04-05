a9e8e7a6b65beeecbeaf85e84adaac2f817bec28
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-05 21:08:15 +0800

    typed-holes: screenkey is in 18.03

--- typed-holes/idris/screenkey.sh
+++ typed-holes/idris/screenkey.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/b828d00ffdae51ee44e04f2104d18eeee75344a0.tar.gz
 #! nix-shell -i bash -p screenkey
 
 screenkey -t 0.5
