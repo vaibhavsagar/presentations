71cd4d8163ac4a935a7de24cbe311847d48455ec
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-11 11:00:14 +0800

    .nix/default.nix: use .nix/pkgs.nix

--- .nix/default.nix
+++ .nix/default.nix
@@ -1,20 +1,13 @@
 let
-  inherit (import <nixpkgs> {}) lib;
-  fetcher = { owner, repo, rev, sha256 }: builtins.fetchTarball {
-    inherit sha256;
-    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-  };
-  versions = lib.mapAttrs
-    (_: fetcher)
-    (builtins.fromJSON (builtins.readFile ./versions.json));
-  pkgs = import versions.nixpkgs {};
-in name: src: pkgs.runCommand name {
+  pkgs    = import ./pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+in name: src: nixpkgs.runCommand name {
   buildInputs = [
-    pkgs.haskellPackages.pandoc
-    pkgs.python3
+    nixpkgs.haskellPackages.pandoc
+    nixpkgs.python3
   ];
 } ''
   mkdir -p $out
   pandoc --standalone -t revealjs -V theme:simple ${src + "/presentation.md"} -o $out/index.html
-  cp -R ${versions.revealjs} $out/reveal.js
+  cp -R ${pkgs.revealjs} $out/reveal.js
 ''
