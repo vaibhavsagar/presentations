753d33ba89f084fd302ce4e3fb8a8fd429e76443
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-28 04:59:00 +0800

    typed-holes/haskell/shell.nix: don't override all-cabal-hashes

--- typed-holes/haskell/shell.nix
+++ typed-holes/haskell/shell.nix
@@ -1,10 +1,5 @@
 let
   overlay = self: super: {
-    all-cabal-hashes = self.fetchurl {
-      url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/61de1110af16fa0645a8cc8bbaf4a9c6fca9d8e8.tar.gz";
-      sha256 = "0vwfm1imwcffyliaama572ksjgqmwxk48py0wgvgpi2vg08w7a12";
-    };
-
     haskellPackages = super.haskellPackages.extend ( self_: super_: {
       extra = self_.callHackage "extra" "1.6.6" {};
       ghcid = self_.callHackage "ghcid" "0.7" {};
