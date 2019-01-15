c00e1781096962f1fdcd89b64182d664f74ff438
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-01-14 22:43:26 -0500

    tarjans-scc: add images

--- tarjans-scc/default.nix
+++ tarjans-scc/default.nix
@@ -1,1 +1,12 @@
-import ../.nix/default.nix { name = "tarjans-scc"; src = ./.; }
+let
+  pkgs    = import ../.nix/pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+  demo = nixpkgs.lib.sourceByRegex ./demo [".*\.svg$"];
+  command = src: pkgs: ''
+    mkdir -p $out
+    pandoc --standalone -t revealjs -V theme:simple ${src + "/presentation.md"} -o $out/index.html
+    cp -R ${pkgs.revealjs} $out/reveal.js
+    ln -s ${demo} $out/demo
+  '';
+in
+import ../.nix/default.nix { inherit command; name = "tarjans-scc"; src = ./.; }
