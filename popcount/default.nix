1557021c1a8e4864a6bd32570b0eb6814a72fdaa
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-04-28 19:47:22 -0400

    popcount: copy images

--- popcount/default.nix
+++ popcount/default.nix
@@ -1,7 +1,12 @@
-let command = src: pkgs: ''
+let
+  pkgs    = import ../.nix/pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+  images  = nixpkgs.lib.sourceByRegex ./images [ ".*\.svg$" ".*\.png$" ];
+  command = src: pkgs: ''
     mkdir -p $out
     pandoc --standalone -t revealjs -V theme:solarized ${src + "/presentation.md"} -o $out/index.html
     cp -R ${pkgs.revealjs} $out/reveal.js
+    ln -s ${images} $out/images
   '';
 in
 import ../.nix/default.nix { inherit command; name = "popcount"; src = ./.; }
