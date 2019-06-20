128a086d2e5f17a06431f331d06dea7d5c9e7a87
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-06-20 18:47:19 -0400

    javascript-in-haskell: use solarized

--- javascript-in-haskell/default.nix
+++ javascript-in-haskell/default.nix
@@ -1,1 +1,10 @@
-import ../.nix/default.nix { name = "javascript-in-haskell"; src = ./.; }
+let
+  pkgs    = import ../.nix/pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+  command = src: pkgs: ''
+    mkdir -p $out
+    pandoc --standalone -t revealjs -V theme:solarized ${src + "/presentation.md"} -o $out/index.html
+    cp -R ${pkgs.revealjs} $out/reveal.js
+  '';
+in
+import ../.nix/default.nix { inherit command; name = "javascript-in-haskell"; src = ./.; }
