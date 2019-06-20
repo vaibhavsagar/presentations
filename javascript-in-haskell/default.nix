9d3278d0db3ac419382138ca40d4a642172945b0
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-06-20 17:28:37 -0400

    javascript-in-haskell: no images

--- javascript-in-haskell/default.nix
+++ javascript-in-haskell/default.nix
@@ -1,12 +1,1 @@
-let
-  pkgs    = import ../.nix/pkgs.nix;
-  nixpkgs = import pkgs.nixpkgs {};
-  images  = nixpkgs.lib.sourceByRegex ./images [ ".*\.svg$" ".*\.png$" ];
-  command = src: pkgs: ''
-    mkdir -p $out
-    pandoc --standalone -t revealjs -V theme:solarized ${src + "/presentation.md"} -o $out/index.html
-    cp -R ${pkgs.revealjs} $out/reveal.js
-    ln -s ${images} $out/images
-  '';
-in
-import ../.nix/default.nix { inherit command; name = "javascript-in-haskell"; src = ./.; }
+import ../.nix/default.nix { name = "javascript-in-haskell"; src = ./.; }
