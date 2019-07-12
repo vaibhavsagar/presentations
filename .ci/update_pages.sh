4cbe3d1e8173cb484b02d0a1c9fbb03b49df0ece
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-07-12 02:10:02 -0400

    .ci/update_pages.sh: use improvements from website experimentation

--- .ci/update_pages.sh
+++ .ci/update_pages.sh
@@ -4,7 +4,7 @@ TARGET_REPO=vaibhavsagar/presentations.git
 OUTPUT_FOLDER=result
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
+    echo -e "Starting to deploy to Github Pages\\n"
     if [ "$TRAVIS" == "true" ]; then
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "Travis"
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git clone --depth 1 --quiet --branch=$BRANCH "https://$GH_TOKEN@github.com/$TARGET_REPO" build > /dev/null 2>&1
     # Go into directory and copy data we're interested in to that directory
     cd build || exit 1
-    rsync -avL --delete --exclude=.git  ../$OUTPUT_FOLDER/ ./
+    rsync -avvL --inplace --no-whole-file --delete --exclude=.git  ../$OUTPUT_FOLDER/ ./
     # Add, commit and push files
     git add --all .
     git commit --allow-empty -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
