ba610c530446a4269cdf553e397b89a043061e92
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-07-12 16:22:01 -0400

    .nix/pkgs.nix: don't use <nixpkgs>

--- .nix/pkgs.nix
+++ .nix/pkgs.nix
@@ -1,10 +1,10 @@
 let
-  inherit (import <nixpkgs> {}) lib;
   fetcher = { owner, repo, rev, sha256, ... }: builtins.fetchTarball {
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
   };
-  versions = lib.mapAttrs
+  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
+  versions = nixpkgs.lib.mapAttrs
     (_: fetcher)
     (builtins.fromJSON (builtins.readFile ./versions.json));
 in versions
