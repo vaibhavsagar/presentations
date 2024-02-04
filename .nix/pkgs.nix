9166130235136bc88643260b0e01f1299702c3ea
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-02-05 02:10:00 +1100

    .nix/pkgs.nix: use builtins.mapAttr

--- .nix/pkgs.nix
+++ .nix/pkgs.nix
@@ -3,8 +3,7 @@ let
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
   };
-  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
-  versions = nixpkgs.lib.mapAttrs
+  versions = builtins.mapAttrs
     (_: fetcher)
     (builtins.fromJSON (builtins.readFile ./versions.json));
 in versions
