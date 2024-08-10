ea5825c8f6a48227f3f4957d002d044db6fc251c
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-08-11 00:51:43 +1000

    .nix/default.nix: add extraBuildInputs

--- .nix/default.nix
+++ .nix/default.nix
@@ -5,10 +5,10 @@ let
     mkdir -p $out
     pandoc --standalone -t revealjs -V theme:simple ${src + "/presentation.md"} -A ${../tracking.html} -o $out/index.html
   '';
-in { command ? defaultCommand, name, src }: nixpkgs.runCommand name {
+in { command ? defaultCommand, name, src, extraBuildInputs ? (_: []) }: nixpkgs.runCommand name {
   buildInputs = [
     nixpkgs.entr
     nixpkgs.pandoc
     nixpkgs.haskellPackages.wai-app-static
-  ];
+  ] ++ (extraBuildInputs nixpkgs);
 } (command src pkgs)
