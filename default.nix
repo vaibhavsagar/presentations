f94793e78a7539b70bcbd4b65351a7685a5766d9
Vaibhav Sagar <vaibhavsagar@gmail.com>
2026-06-23 00:06:49 +1000

    default.nix: add index to presentations

--- default.nix
+++ default.nix
@@ -1,22 +1,55 @@
 let
   pkgs    = import .nix/pkgs.nix;
   nixpkgs = import pkgs.nixpkgs {};
-  presentations = map (folder: {
+  presentation = folder: {
     name = folder;
     path = import (./. + "/${folder}");
-  });
-in nixpkgs.linkFarm "presentations" (presentations [
-  "functional-devops"
-  "functional-ingsoc"
-  "how-to-stop-writing-haskell"
-  "ihaskell-can-do-that"
-  "javascript-in-haskell"
-  "lazy-functional-state-threads"
-  "minimal-perfect-hashing"
-  "partial-evaluation"
-  "popcount"
-  "sat-solvers"
-  "space-efficient-static-trees-graphs"
-  "tarjans-scc"
-  "typed-holes"
-])
+  };
+  folders = [
+    "functional-devops"
+    "functional-ingsoc"
+    "how-to-stop-writing-haskell"
+    "ihaskell-can-do-that"
+    "javascript-in-haskell"
+    "lazy-functional-state-threads"
+    "minimal-perfect-hashing"
+    "partial-evaluation"
+    "popcount"
+    "sat-solvers"
+    "space-efficient-static-trees-graphs"
+    "tarjans-scc"
+    "typed-holes"
+  ];
+  index = names: let
+    entry = folder: ''<li><a href="./${folder}/">${folder}</li>'';
+    header = ''
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Presentations</title>
+      </head>
+    '';
+    body = nixpkgs.lib.strings.concatLines ([
+      "<body>"
+      "<h1>Presentations</h1>"
+      "<ul>"
+    ] ++ (map entry names) ++ [
+      "</ul>"
+      "</body>"
+    ]);
+    footer = "</html>";
+  in nixpkgs.writeText "index.html" (nixpkgs.lib.strings.concatStrings [
+    header
+    body
+    footer
+  ]);
+in nixpkgs.symlinkJoin {
+  name = "presentations";
+  paths = [
+    (nixpkgs.linkFarm "presentations" (map presentation folders))
+  ];
+  postBuild = ''
+    install ${index folders} $out/index.html
+  '';
+}
