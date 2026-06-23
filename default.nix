8b73af696b91b77f640aceb280e673a3e3dd57e4
Vaibhav Sagar <vaibhavsagar@gmail.com>
2026-06-23 13:59:26 +1000

    default.nix: add tracking to index

--- default.nix
+++ default.nix
@@ -28,6 +28,8 @@ let
       <head>
         <meta charset="UTF-8">
         <title>Presentations</title>
+        <script data-goatcounter="https://vaibhavsagar.goatcounter.com/count"
+                async src="//gc.zgo.at/count.js"></script>
       </head>
     '';
     body = nixpkgs.lib.strings.concatLines ([
